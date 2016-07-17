232bf896e2f77d302a2c8ae7f870562d6ea35310
Steven Wolfe <steven@swolfe.me>
2016-07-17 14:38:36 -0400

    Parsing all career stats in maps

--- goow/player_stats.go
+++ goow/player_stats.go
@@ -22,19 +22,20 @@ type PlayerStats struct {
 
 // statsCollection : Holds a collection of stats for a particular player
 type statsCollection struct {
-	EliminationsAvg   float64                    `json:"eliminationsAvg"`
-	DamageDoneAvg     int64                      `json:"damageDoneAvg"`
-	DeathsAvg         float64                    `json:"deathsAvg"`
-	FinalBlowsAvg     float64                    `json:"finalBlowsAvg"`
-	HealingDoneAvg    int64                      `json:"healingDoneAvg"`
-	ObjectiveKillsAvg float64                    `json:"objectiveKillsAvg"`
-	ObjectiveTimeAvg  string                     `json:"objectiveTimeAvg"`
-	SoloKillsAvg      float64                    `json:"soloKillsAvg"`
-	TopHeros          map[string]*basicHeroStats `json:"topHeros"`
+	EliminationsAvg   float64                  `json:"eliminationsAvg"`
+	DamageDoneAvg     int64                    `json:"damageDoneAvg"`
+	DeathsAvg         float64                  `json:"deathsAvg"`
+	FinalBlowsAvg     float64                  `json:"finalBlowsAvg"`
+	HealingDoneAvg    int64                    `json:"healingDoneAvg"`
+	ObjectiveKillsAvg float64                  `json:"objectiveKillsAvg"`
+	ObjectiveTimeAvg  string                   `json:"objectiveTimeAvg"`
+	SoloKillsAvg      float64                  `json:"soloKillsAvg"`
+	TopHeros          map[string]*topHeroStats `json:"topHeros"`
+	CareerStats       map[string]*careerStats  `json:"careerStats"`
 }
 
-// basicHeroStats : Holds specific stats for each hero
-type basicHeroStats struct {
+// topHeroStats : Holds basic stats for each hero
+type topHeroStats struct {
 	TimePlayed          string  `json:"timePlayed"`
 	GamesWon            int     `json:"gamesWon"`
 	WinPercentage       int     `json:"winPercentage"`
@@ -42,6 +43,19 @@ type basicHeroStats struct {
 	EliminationsPerLife float64 `json:"eliminationsPerLife"`
 	MultiKillBest       int     `json:"multiKillBest"`
 	ObjectiveKillsAvg   float64 `json:"objectiveKillsAvg"`
+}
+
+// careerStats : Holds very detailed stats for each hero
+type careerStats struct {
+	Assists       map[string]string `json:"assists,omitempty"`
+	Average       map[string]string `json:"average,omitempty"`
+	Best          map[string]string `json:"best,omitempty"`
+	Combat        map[string]string `json:"combat,omitempty"`
+	Deaths        map[string]string `json:"deaths,omitempty"`
+	HeroSpecific  map[string]string `json:"heroSpecific,omitempty"`
+	Game          map[string]string `json:"game,omitempty"`
+	MatchAwards   map[string]string `json:"matchAwards,omitempty"`
+	Miscellaneous map[string]string `json:"miscellaneous,omitempty"`
 }
 
 // GetPlayerStats : Gets all stats available for a player
@@ -112,12 +126,15 @@ func parseDetailedStats(playModeSelector *goquery.Selection) statsCollection {
 	// Parses out top hero stats and assigns it to our parent struct
 	sc.TopHeros = parseHeroStats(playModeSelector.Find("section.hero-comparison-section").First())
 
+	// Parses out career stats and assigns it to our parent struct
+	sc.CareerStats = parseCareerStats(playModeSelector.Find("section.career-stats-section").First())
+
 	return sc
 }
 
 // parseHeroStats : Parses stats for each individual hero and returns a map
-func parseHeroStats(heroStatsSelector *goquery.Selection) map[string]*basicHeroStats {
-	bhsMap := make(map[string]*basicHeroStats)
+func parseHeroStats(heroStatsSelector *goquery.Selection) map[string]*topHeroStats {
+	bhsMap := make(map[string]*topHeroStats)
 
 	heroStatsSelector.Find("div.progress-category").Each(func(i int, heroGroupSel *goquery.Selection) {
 		categoryID, _ := heroGroupSel.Attr("data-category-id")
@@ -128,7 +145,7 @@ func parseHeroStats(heroStatsSelector *goquery.Selection) map[string]*basicHeroS
 
 			// Creates hero map if it doesn't exist
 			if bhsMap[heroName] == nil {
-				bhsMap[heroName] = new(basicHeroStats)
+				bhsMap[heroName] = new(topHeroStats)
 			}
 
 			// Sets hero stats based on stat category type
@@ -152,4 +169,99 @@ func parseHeroStats(heroStatsSelector *goquery.Selection) map[string]*basicHeroS
 	})
 
 	return bhsMap
+}
+
+func parseCareerStats(careerStatsSelector *goquery.Selection) map[string]*careerStats {
+	csMap := make(map[string]*careerStats)
+
+	heroMap := make(map[string]string)
+	// Populates tempHeroMap to match hero ID to name in second scrape
+	careerStatsSelector.Find("select.js-career-select option").Each(func(i int, heroSel *goquery.Selection) {
+		heroVal, _ := heroSel.Attr("value")
+		heroMap[heroVal] = heroSel.Text()
+	})
+
+	// Iterates over every hero div
+	careerStatsSelector.Find("div.row div.js-stats").Each(func(i int, heroStatsSel *goquery.Selection) {
+		currentHero, _ := heroStatsSel.Attr("data-category-id")
+		currentHero = heroMap[currentHero]
+
+		// Iterates over every stat box
+		heroStatsSel.Find("div.column.xs-12").Each(func(i2 int, statBoxSel *goquery.Selection) {
+			statType := statBoxSel.Find("span.stat-title").Text()
+			statType = strings.ToLower(statType)
+
+			// Iterates over stat row
+			statBoxSel.Find("table.data-table tbody tr").Each(func(i3 int, statSel *goquery.Selection) {
+
+				// Iterates over every stat td
+				statKey := ""
+				statVal := ""
+				statSel.Find("td").Each(func(i4 int, statKV *goquery.Selection) {
+					switch i4 {
+					case 0:
+						statKey = statKV.Text()
+					case 1:
+						statVal = statKV.Text()
+
+						// Creates stat map if it doesn't exist
+						if csMap[currentHero] == nil {
+							csMap[currentHero] = new(careerStats)
+						}
+
+						// Switches on type, creating category stat maps if exists (will omitempty on json marshal)
+						switch statType {
+						case "assists":
+							if csMap[currentHero].Assists == nil {
+								csMap[currentHero].Assists = make(map[string]string)
+							}
+							csMap[currentHero].Assists[statKey] = statVal
+						case "average":
+							if csMap[currentHero].Average == nil {
+								csMap[currentHero].Average = make(map[string]string)
+							}
+							csMap[currentHero].Average[statKey] = statVal
+						case "best":
+							if csMap[currentHero].Best == nil {
+								csMap[currentHero].Best = make(map[string]string)
+							}
+							csMap[currentHero].Best[statKey] = statVal
+						case "combat":
+							if csMap[currentHero].Combat == nil {
+								csMap[currentHero].Combat = make(map[string]string)
+							}
+							csMap[currentHero].Combat[statKey] = statVal
+						case "deaths":
+							if csMap[currentHero].Deaths == nil {
+								csMap[currentHero].Deaths = make(map[string]string)
+							}
+							csMap[currentHero].Deaths[statKey] = statVal
+						case "hero specific":
+							if csMap[currentHero].HeroSpecific == nil {
+								csMap[currentHero].HeroSpecific = make(map[string]string)
+							}
+							csMap[currentHero].HeroSpecific[statKey] = statVal
+						case "game":
+							if csMap[currentHero].Game == nil {
+								csMap[currentHero].Game = make(map[string]string)
+							}
+							csMap[currentHero].Game[statKey] = statVal
+						case "match awards":
+							if csMap[currentHero].MatchAwards == nil {
+								csMap[currentHero].MatchAwards = make(map[string]string)
+							}
+							csMap[currentHero].MatchAwards[statKey] = statVal
+						case "miscellaneous":
+							if csMap[currentHero].Miscellaneous == nil {
+								csMap[currentHero].Miscellaneous = make(map[string]string)
+							}
+							csMap[currentHero].Miscellaneous[statKey] = statVal
+						}
+					}
+				})
+			})
+		})
+	})
+
+	return csMap
 }
